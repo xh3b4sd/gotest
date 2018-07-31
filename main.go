5a95c32f4249ffff3cbca768c061b237ee44dc31
Tim Schindler <xh3b4sd@gmail.com>
2018-07-31 21:48:05 +0200

    adds script to rewrite microerror errors in projects

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/giantswarm/microerror"
+)
+
+const path = "/Users/xh3b4sd/go/src/github.com/giantswarm/e2etests"
+
+const search = `var ([a-zA-Z]+) = microerror.New\("[ a-zA-Z]+"\)`
+
+const replace = `var $1 = &microerror.Error{
+	Kind: "$1",
+}`
+
+func main() {
+	walkFunc := func(p string, i os.FileInfo, err error) error {
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if i.IsDir() && i.Name() == ".circleci" {
+			return filepath.SkipDir
+		}
+		if i.IsDir() && i.Name() == ".git" {
+			return filepath.SkipDir
+		}
+		if i.IsDir() && i.Name() == "vendor" {
+			return filepath.SkipDir
+		}
+		if i.IsDir() {
+			return nil
+		}
+		if !i.IsDir() && i.Name() != "error.go" {
+			return nil
+		}
+
+		c, err := ioutil.ReadFile(p)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r, err := regexp.Compile(search)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		n := r.ReplaceAllString(string(c), replace)
+
+		err = ioutil.WriteFile(p, []byte(n), i.Mode())
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		return nil
+	}
+
+	err := filepath.Walk(path, walkFunc)
+	if err != nil {
+		panic(err)
+	}
+}
