8ab36bd911967434bcd11cd666a866a7a11e894b
Tim Schindler <xh3b4sd@gmail.com>
2018-08-24 16:19:20 +0200

    adds main.go

--- main.go
+++ main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
-	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/client/k8srestconfig"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 )
-
-const (
-	SelfLink = "/apis/provider.giantswarm.io/v1alpha1/namespaces/default/awsconfigs/6aben/status"
-)
-
-type Patch struct {
-	Op    string      `json:"op"`
-	Path  string      `json:"path"`
-	Value interface{} `json:"value"`
-}
-
-type Status struct {
-	Cluster providerv1alpha1.StatusCluster `json:"cluster" yaml:"cluster"`
-}
 
 func main() {
 	var err error
@@ -45,12 +29,12 @@ func main() {
 		c := k8srestconfig.Config{
 			Logger: logger,
 
-			Address:   "https://127.0.0.1:52565",
+			Address:   "https://127.0.0.1:53078",
 			InCluster: false,
 			TLS: k8srestconfig.TLSClientConfig{
-				CAFile:  "/Users/xh3b4sd/.config/opsctl/gauss/certs/1508360771/ca.pem",
-				CrtFile: "/Users/xh3b4sd/.config/opsctl/gauss/certs/1508360771/crt.pem",
-				KeyFile: "/Users/xh3b4sd/.config/opsctl/gauss/certs/1508360771/key.pem",
+				CAFile:  "/Users/xh3b4sd/.config/opsctl/ginger/certs/1534240785/ca.pem",
+				CrtFile: "/Users/xh3b4sd/.config/opsctl/ginger/certs/1534240785/crt.pem",
+				KeyFile: "/Users/xh3b4sd/.config/opsctl/ginger/certs/1534240785/key.pem",
 			},
 		}
 
@@ -60,54 +44,20 @@ func main() {
 		}
 	}
 
-	var restClient rest.Interface
-	{
-		g8sClient, err := versioned.NewForConfig(restConfig)
-		if err != nil {
-			panic(err)
-		}
-		restClient = g8sClient.ProviderV1alpha1().RESTClient()
-	}
-
-	obj, err := restClient.Get().AbsPath(SelfLink).Do().Get()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%#v\n", obj)
-
-	var patches []Patch
-
-	patches = append(patches, Patch{
-		Op:   "add",
-		Path: "/status",
-		Value: Status{
-			Cluster: providerv1alpha1.StatusCluster{
-				Conditions: []providerv1alpha1.StatusClusterCondition{},
-				Nodes:      []providerv1alpha1.StatusClusterNode{},
-				Versions:   []providerv1alpha1.StatusClusterVersion{},
-			},
-		},
-	})
-
-	b, err := json.Marshal(patches)
+	g8sClient, err := versioned.NewForConfig(restConfig)
 	if err != nil {
 		panic(err)
 	}
 
-	err = restClient.Patch(types.JSONPatchType).AbsPath(SelfLink).Body(b).Do().Error()
-	if errors.IsConflict(err) {
-		panic(err)
-	} else if errors.IsResourceExpired(err) {
-		panic(err)
+	n := ""
+	o := metav1.GetOptions{}
+	_, err = g8sClient.ProviderV1alpha1().AWSConfigs(n).Get("", o)
+	if errors.IsBadRequest(err) {
+		fmt.Printf("IsBadRequest\n")
+	} else if errors.IsInvalid(err) {
+		fmt.Printf("IsInvalid\n")
 	} else if err != nil {
+		fmt.Printf("%#v\n", err)
 		panic(err)
 	}
-
-	fmt.Printf("\n")
-
-	obj, err = restClient.Get().AbsPath(SelfLink).Do().Get()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%#v\n", obj)
 }
